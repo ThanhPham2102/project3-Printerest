4d0b54dcabbf115f1832467b78d22060e1b2c039
ThanhPham2102 <20133489@student.hust.edu.vn>
2022-11-07 08:57:07 +0700

    thanh

--- models/db.js
+++ models/db.js
@@ -3,6 +3,6 @@ let pool = mysql.createPool({
   host: "localhost",
   user: "root",
   database: "user_pint",
-  password: "thanh1995@",
+  password: "thanhpham1995@",
 });
 module.exports = pool.promise();
